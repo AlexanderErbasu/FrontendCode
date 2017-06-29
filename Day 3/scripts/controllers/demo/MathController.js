6bf0618173d650523b121b3d5d57c354ef3e1dfc
alexandru.erbasu <erbasualex@gmail.com>
2017-06-29 11:50:48 +0300

    Until chapter III

--- Day 3/scripts/controllers/demo/MathController.js
+++ Day 3/scripts/controllers/demo/MathController.js
@@ -1,8 +1,11 @@
 hrApp.controller('MathController', ['$scope', function($scope){
-    
+
     $scope.calculate = function() {
 //        TODO #8 calculate op1, op2, op3, op4
-
+        $scope.op1=$scope.numA+$scope.numB;
+        $scope.op2=$scope.numA-$scope.numB;
+        $scope.op3=$scope.numA*$scope.numB;
+        $scope.op4=$scope.numA/$scope.numB;
 //        TODO #13 refactor your calculations using MathService
     }
 
